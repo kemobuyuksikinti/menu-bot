20121c0a06a2b23ff60f72c8cb3f3b5ff3592de0
Kemosalvo <111445987+kemobuyuksikinti@users.noreply.github.com>
2022-08-19 11:58:48 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Menu Rol Bot
+
+# Kurulum
+* İlk olarak bilgisayarına [Node JS](https://nodejs.org/en/) indir.
+* Bu projeyi zip halinde indir.
+* Herhangi bir klasöre zipi çıkart.
+* yanak.js dosyasını sunucuya göre düzenle.
+* Sonra klasörün içerisinde bir `powershell` ya da `cmd` penceresi aç.
+* ```npm install``` yazarak tüm modülleri kur.
+* Kurulum bittikten sonra ```npm start``` yaz ve botu başlat.
+
+# Hakkında
+* Bot emircim#0001 ile kemosalvo#1943 kodlamıştır.
---
+++ yanak.js
@@ -0,0 +1,80 @@
+module.exports = {
+    "Bot": {
+        "ClientToken": "MTAwNTUxMTg4NTU3MzA3MDkxOA.Gb-BvG.CcORwUIB-P-_VrguygZagRHYn9friGZGQO58hg",
+        "BotVoiceChannel": "1005482731540582400",
+        "Command": "yanakbabapro123kemosalvopro123",
+        "ServerName": "SALVO #ŞAMPİYONLARLİGİ",
+        "Prefix": "!",
+
+        "Ready": {
+            "Name": "S A L V O",
+            "Type": "S A L V O",
+            "Status": "Idled",
+            "Twitch": ""
+        }
+    },
+
+
+    "Rol": {
+        "Alone": "1005504099149697034",
+        "Lovers": "1005504099871117363",
+        "NoLove": "1005504100600922122",
+
+        "Mavi": "1005513394100637727",
+        "Yeşil": "1005513394918539395",
+        "Sari": "1005513395639955516",
+        "Kirmizi": "1005513396420083853",
+        "Turuncu": "1005513396915023892",
+        "Mor": "1005513397221195948",
+        "Pembe": "1005513398370451526",
+
+        "Çekiliş": "1005504097790738483",
+        "Etkinlik": "1005504098625388584"
+    },
+
+
+    "Button": {
+        "Labels": {
+            "AloneL": "Alone 💔",
+            "LoversL": "Lovers 💖",
+            "NoLoveL": "Sevgili Yapmıyorum 🚫",
+
+            "Mavi": "Mavi 💙",
+            "Yeşil": "Yeşil 💚",
+            "Sari": "Sarı 💛",
+            "Kirmizi": "Kırmızı ❤️",
+            "Turuncu": "Turuncu 🧡",
+            "Mor": "Mor 💜",
+            "Pembe": "Pembe 🟣",
+
+            "Çekiliş": "Çekiliş Katılımcısı 🎁",
+            "Etkinlik": "Etkinlik Katılımcısı 🎉"
+        },
+
+        "Emoji": {
+            "AloneE": "💔",
+            "LoversE": "💖",
+            "NoLoveE": "🚫",
+
+            "Mavi": "💙",
+            "Yeşil": "💚",
+            "Sari": "💛",
+            "Kirmizi": "❤️",
+            "Turuncu": "🧡",
+            "Mor": "💜",
+            "Pembe": "🟣",
+
+            "Çekiliş": "🎁",
+            "Etkinlik": "🎉",
+
+            "Nitro": "<:salvo_nitro:1005516111573430327>",
+            "Netflix": "<:salvo_netflix:1005516440327172146>",
+            "Exxen": "<:salvo_exxen:1005518333552115803>",
+            "BluTV": "<:salvo_blutv:1005517844009734265>",
+            "Spotify": "<:salvo_spotify:1005516652282122301>"
+        }
+    }
+
+    
+
+};
